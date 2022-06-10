2934305ed07c853a2e4ad887b47042953369252d
Rohankadlak <107106886+Rohankadlak@users.noreply.github.com>
2022-06-10 11:34:08 +0530

    Update sum.c

--- Maths/sum.c
+++ Maths/sum.c
@@ -2,13 +2,18 @@
 
 int sum(int a,int b);
 int sub(int a,int b);
+int add(int a, int b){
+	return a+b;
+}
 int main(){
 	int a=10;
 	int b=10;
         sum=0;
 	int c=12;
         sum(a,c);
+	add(a,b);
 
-nothing to do	sum(a,b);
+nothing to do
+	return 0;
 }
 
